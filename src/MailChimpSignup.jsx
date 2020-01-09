1878d087ac95f45d55204f2733f04b31a6d0b7c4
Deeter <deeter.cesler@gmail.com>
2020-01-08 17:16:25 -0700

    resolved errors and also input width on mobile

--- src/MailChimpSignup.jsx
+++ src/MailChimpSignup.jsx
@@ -5,21 +5,21 @@ const MailChimpSignup = () => {
         <div>
         {/* <!-- Begin Mailchimp Signup Form --> */}
         <div id="mc_embed_signup">
-        <form action="https://bageldogmedia.us16.list-manage.com/subscribe/post?u=c1fa698dbe7a1b924b1ae68b9&amp;id=15bfa656ae" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <form action="https://bageldogmedia.us16.list-manage.com/subscribe/post?u=c1fa698dbe7a1b924b1ae68b9&amp;id=15bfa656ae" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
             <div id="mc_embed_signup_scroll">
             <h2>get a weekly idea to push your thinking</h2>
-        {/* <div class="indicates-required"><span class="asterisk">*</span> indicates required</div> */}
-        <div class="mc-field-group">
-            <label for="mce-EMAIL"></label>
-            <input type="email" placeholder="What's your email address?" name="EMAIL" class="required email" id="mce-EMAIL"/>
+        {/* <div className="indicates-required"><span className="asterisk">*</span> indicates required</div> */}
+        <div className="mc-field-group">
+            <label htmlFor="mce-EMAIL"></label>
+            <input type="email" placeholder="What's your email address?" name="EMAIL" className="required email" id="mce-EMAIL"/>
         </div>
-            <div id="mce-responses" class="clear">
-                <div class="response" id="mce-error-response"></div>
-                <div class="response" id="mce-success-response"></div>
+            <div id="mce-responses" className="clear">
+                <div className="response" id="mce-error-response"></div>
+                <div className="response" id="mce-success-response"></div>
             </div>    
             {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-            <div className="mailchimp-style" aria-hidden="true"><input type="text" name="b_c1fa698dbe7a1b924b1ae68b9_15bfa656ae" tabindex="-1" value=""/></div>
-            <div class="clear"><input className="mailchimp-submit" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/></div>
+            <div className="mailchimp-style" aria-hidden="true"><input type="text" name="b_c1fa698dbe7a1b924b1ae68b9_15bfa656ae" tabIndex="-1" defaultValue=""/></div>
+            <div className="clear"><input className="mailchimp-submit" type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
             </div>
         </form>
 

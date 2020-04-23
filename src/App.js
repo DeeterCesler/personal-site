5b501e355ff241adede428bfa7a0a626554cf096
Deeter <deeter.cesler@gmail.com>
2020-04-23 17:22:03 -0600

    add google analytics

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import Work from './Work';
 import Now from './Now';
 import Blog from './Blog';
 import Singleblog from './Singleblog';
-// import ReactGA from 'react-ga';
-// ReactGA.initialize('UA-110417068-2');
-// ReactGA.pageview(window.location.pathname + window.location.search);
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-110417068-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const backendURL = process.env.REACT_APP_BACKEND_SERVER_ADDRESS
 

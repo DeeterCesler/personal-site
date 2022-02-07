9123d8df4492e4c777b29e497dd82be07453146a
Deeter <deeter.cesler@gmail.com>
2022-02-07 18:22:37 -0500

    cleaning up formatting, class names

--- src/Fun.jsx
+++ src/Fun.jsx
@@ -3,24 +3,26 @@ import React from "react";
 const Fun = () => {
     return(
         <div className="now">
-            <div className="spacer"/>
-            {/* <div className="disappearing-spacer"/> */}
-            <h1 className="fun-header">FUN</h1>
-            <br/>
-            <br/>
-            <div className="">
-                <div className="readability">
-                    <h3><a className="link" href="http://dungeon-crawler.freeoda.com/" style= {{ textDecoration: "none" }}>DUNGEON CRAWLER</a></h3>
-                    <p>browser-based game I made while learning jQuery. keyboard required</p>
-                    <br/>
-                    <br/>
-                    <h3><a className="link" href="https://happy-minsky-a19081.netlify.app/" style= {{ textDecoration: "none" }}>LASER EYES MAKER</a></h3>
-                    <p>add laser eyes to your profile pic</p>
-                    <br/>
+            <div className="container">
+                <div className="spacer"/>
+                {/* <div className="disappearing-spacer"/> */}
+                <h1 className="header">FUN</h1>
+                <br/>
+                <br/>
+                <div className="">
+                    <div className="readability">
+                        <h3><a className="link" href="http://dungeon-crawler.freeoda.com/" style= {{ textDecoration: "none" }}>DUNGEON CRAWLER</a></h3>
+                        <p>browser-based game I made while learning jQuery. keyboard required</p>
+                        <br/>
+                        <br/>
+                        <h3><a className="link" href="https://happy-minsky-a19081.netlify.app/" style= {{ textDecoration: "none" }}>LASER EYES MAKER</a></h3>
+                        <p>add laser eyes to your profile pic</p>
+                        <br/>
+                    </div>
                 </div>
+                <div className="spacer"/>
+                <div className="spacer"/>
             </div>
-            <div className="spacer"/>
-            <div className="spacer"/>
         </div>
     )
 }
--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -2,21 +2,23 @@ import React from "react";
 
 const NotFoundPage = () => {
     return(
-<div className="now">
-            <div className="spacer"/>
-            <h1 className="now-header">WHOOPS</h1>
-            <div className="spacer"/>
-            <div className="">
-                <div className="readability">
-                    <h3 className="">YOU GOT LOST</h3>
-                    <br/>
-                    <p className="">or the link you found was old</p>
-                    <p className="small">it happens to everyone</p>
-                    <p className="lost tiny"><a href="/" className="">go back home</a></p>
+        <div className="now">
+            <div className="container">
+                <div className="spacer"/>
+                <h1 className="header">WHOOPS</h1>
+                <div className="spacer"/>
+                <div className="">
+                    <div className="main">
+                        <h3 className="bold">YOU GOT LOST</h3>
+                        <br/>
+                        <p className="">or the link you found was old</p>
+                        <p className="small">it happens to everyone</p>
+                        <p className="lost tiny"><a href="/" className="">go back home</a></p>
+                    </div>
                 </div>
+                <div className="spacer"/>
+                <div className="spacer"/>
             </div>
-            <div className="spacer"/>
-            <div className="spacer"/>
         </div>
     )
 }

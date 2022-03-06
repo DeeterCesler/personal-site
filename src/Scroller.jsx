00bb6ae33dcbe387e73f08eefc5648a2cbce6c7b
Deeter <deeter.cesler@gmail.com>
2022-03-06 00:56:42 -0500

    fix up scroller, optimize it for mobile. clean up dead code

--- src/Scroller.jsx
+++ src/Scroller.jsx
@@ -3,7 +3,6 @@ import $ from "jquery";
 
 export default function Scroller(){
 
-
     const maxHeight = Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight,
         document.body.offsetHeight, document.documentElement.offsetHeight,
@@ -13,32 +12,24 @@ export default function Scroller(){
     $(window).scroll(function() {
         
         const baseHeight = $(window).height();
-        console.log(baseHeight);
         const currentHeight = $(window).scrollTop() + baseHeight
         const scrollPercentage = ((currentHeight-baseHeight)/(maxHeight-baseHeight)*100);
 
-        $(".insider").css(`height`, `${scrollPercentage}%`);
-
-        $("#circle-lol").attr("stroke-dashoffset", `${315-(scrollPercentage*1.8)}px`);
+        const magicNumber = 315;
+        const magicMultiplier = 1.8;
+        const circleFill = 159;
+        $("#circle-lol").attr("stroke-dashoffset", `${magicNumber-(scrollPercentage*magicMultiplier)}px`);
         
-        
-        if ($(window).scrollTop() + $(window).height() >= 2000) {
-            $(".progress").addClass('red');
-        }
-        if ($(window).scrollTop() <= 100) {
-            $(".progress").removeClass('red');
-        }
+        if(scrollPercentage > 10) $(".white-wrapper").removeClass('hidden').addClass('pop-in');
 
-        if(parseInt($("#circle-lol").attr("stroke-dashoffset")) <= 159) {
-            $(".checkmark").removeClass('hidden');
-            $(".checkmark").addClass('pop-in');
+        const circleProgression = parseInt($("#circle-lol").attr("stroke-dashoffset"));
+        if(circleProgression <= circleFill) {
+            $(".checkmark").removeClass('hidden').addClass('pop-in');
             $("#circle-lol").addClass('grey-out');
-            // $(".checkmark").addClass('fade-out');
         }
 
-        if(parseInt($("#circle-lol").attr("stroke-dashoffset")) > 159) {
-            $(".checkmark").addClass('hidden');
-            $(".checkmark").removeClass('pop-in');
+        if(circleProgression > circleFill) {
+            $(".checkmark").addClass('hidden').removeClass('pop-in');
             $("#circle-lol").removeClass('grey-out');
         }
     });
@@ -55,14 +46,17 @@ export default function Scroller(){
             <div className="checkmark hidden">
                 <svg width="72px" height="72px" viewBox="0 0 100 100" id="emoji" xmlns="http://www.w3.org/2000/svg">
                     <g id="color">
-                    <path fill="#b1cc33" d="m61.5 23.3-8.013-8.013-25.71 25.71-9.26-9.26-8.013 8.013 17.42 17.44z"/>
+                    <path fill="#000" d="m61.5 23.3-8.013-8.013-25.71 25.71-9.26-9.26-8.013 8.013 17.42 17.44z"/>
                     </g>
                     <g id="line">
-                    <path fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2" d="m10.5 39.76 17.42 17.44 33.58-33.89-8.013-8.013-25.71 25.71-9.26-9.26z"/>
+                    <path fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="5" d="m10.5 39.76 17.42 17.44 33.58-33.89-8.013-8.013-25.71 25.71-9.26-9.26z"/>
+                    </g>
+                    <g id="line">
+                    <path fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="1" d="m10.5 39.76 17.42 17.44 33.58-33.89-8.013-8.013-25.71 25.71-9.26-9.26z"/>
                     </g>
                 </svg>
             </div>
-            <div className="white-wrapper">
+            <div className="white-wrapper hidden">
                 <svg viewBox="0 0 100 100" height="150px" xmlns="http://www.w3.org/2000/svg" id="white-circle">
                     <circle strokeLinecap="round"  
                     cx="50" cy="50" r="29" strokeWidth="2" fill="white" strokeDasharray="315" strokeDashoffset="315" />

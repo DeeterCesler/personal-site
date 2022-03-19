93d0055a338a73744b748870e01ebbe1b89591e5
Deeter <deeter.cesler@gmail.com>
2022-03-19 18:30:53 -0400

    update /now from typescript to Rust

--- src/Now.jsx
+++ src/Now.jsx
@@ -7,7 +7,7 @@ const Now = () => {
                 <h1 className="header">NOW</h1>
                 <div className="spacer"/>
                 <div className="main-text">
-                    <p className="now-text">learning <strong>TypeScript</strong></p>
+                    <p className="now-text">learning <strong>Rust</strong> (before, my most recent language was TypeScript)</p>
                     <p className="now-text">product owner @ <a className="white" href="https://sharetalent.co/">share talent</a></p>
                     <p className="now-text">freelance development contracts</p>
                     <p className="now-text">keeping a 900+ day streak on <a className="white" href="https://www.duolingo.com/profile/Deeter">Duolingo</a></p>

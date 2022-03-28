4d0066513870ff21216fcb6fb9fd062c0986ac87
Deeter <deeter.cesler@gmail.com>
2022-03-28 18:15:44 -0400

    add console art

--- src/Logo/index.jsx
+++ src/Logo/index.jsx
@@ -1,14 +1,14 @@
-import React, {Component} from "react";
+import React from "react";
 import home from '../img/home.jpg'
 
-export default class Logo extends Component{
-    render(){
-        return(
-            <div height="250px" id="logo" className="top-nav">
-                <a href="/">
-                    <img src={home} alt="home" />
-                </a>
-            </div>
-        )
-    }
+const Logo = () => {
+    return(
+        <div height="250px" id="logo" className="top-nav">
+            <a href="/">
+                <img src={home} alt="home" />
+            </a>
+        </div>
+    )
 }
+
+export default Logo;

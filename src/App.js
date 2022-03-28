243d0964eb3422e41bd44db0d770bc4b7e5b8772
Deeter <deeter.cesler@gmail.com>
2022-03-28 18:15:44 -0400

    add console art

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './HomePage';
 import Logo from './Logo'
-import Work from './Work';
+// import Work from './Work';
 import Fun from './Fun';
 import Dungeon from './FUN/dungeon';
 import Now from './Now';
@@ -12,134 +12,78 @@ import NotFoundPage from './NotFoundPage';
 ReactGA.initialize('UA-110417068-2');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-const backendURL = process.env.REACT_APP_BACKEND_SERVER_ADDRESS
+const consoleArt = `
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      loggedIn: false,
-      email: "deeter.cesler@gmail.com",
-    }
-  }
+██████╗ ███████╗███████╗████████╗███████╗██████╗ 
+██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+██║  ██║█████╗  █████╗     ██║   █████╗  ██████╔╝
+██║  ██║██╔══╝  ██╔══╝     ██║   ██╔══╝  ██╔══██╗
+██████╔╝███████╗███████╗   ██║   ███████╗██║  ██║
+╚═════╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+                                                 
+ ██████╗███████╗███████╗██╗     ███████╗██████╗  
+██╔════╝██╔════╝██╔════╝██║     ██╔════╝██╔══██╗ 
+██║     █████╗  ███████╗██║     █████╗  ██████╔╝ 
+██║     ██╔══╝  ╚════██║██║     ██╔══╝  ██╔══██╗ 
+╚██████╗███████╗███████║███████╗███████╗██║  ██║ 
+ ╚═════╝╚══════╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝ 
+                                                 `;
 
+const consoleMessage = `
+Welcome! If you're checking the console, you're probably another developer seeing who I am and what I'm about.
+    
+Happy you're here. I will share any code you see that you're interested, if you ask!
 
-  checkForCookie = async () => {
-    // console.log("getting token: ", localStorage.getItem("token"))
-    if(localStorage.getItem("token") !== "null"){
-      try{
-        const targetUrl = backendURL + "auth/verify";
-        console.log("GETTING THIS SHIT, bruh")
-        const getUser = await fetch(targetUrl, {
-          method: 'POST',
-          // body: localStorage.token,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Headers': "POST",
-            'credentials': 'same-origin',
-            "Authorization": localStorage.getItem("token")
-          } 
-        });
-        const parsedResponse = await getUser.json();
-        console.log("parsedREsponse: ", parsedResponse);
-        if(parsedResponse.status === 200){
-          this.setState({
-            loggedIn: true,
-            // email: parsedResponse.email
-          })
-          if(localStorage.getItem("loggedIn") !== "true"){
-            localStorage.setItem("loggedIn", true);
-          }
-          // localStorage.setItem("email", parsedResponse.);
-        } else if (parsedResponse.status === 500){
-          console.log("INTERNAL SERVER ERROR")
-          this.setState({
-            loggedIn: false,
-            // email: ""
-          })
-        } else if (parsedResponse.status === 404){
-          console.log("NO USER FOUND")
-          localStorage.setItem("loggedIn", false);
-          this.setState({
-            loggedIn: false,
-            // email: ""
-          })
-        } else {
-          this.setState({
-            loggedIn: false,
-            // email: ""
-          })
-        }
-      } catch(err){
-        console.log("failed to get cookie", err);
-        localStorage.setItem("loggedIn", false);
-        localStorage.setItem("email", null);
-        localStorage.setItem("token", null);
-        this.setState({
-          loggedIn: false
-        })
-      }
-    } else{
-      // console.log("else statement hit");
-      this.setState({
-        loggedIn: false
-      })
-    }
-    return
-  }
+`;
 
 
-  componentDidMount(){
-    this.checkForCookie();
-  }
+function App(){
 
-  setToken = (token) => {
-    localStorage.setItem("token", token);
-  }
+  useEffect(()=>{
+    console.log(consoleArt);
+    console.log(consoleMessage);
+  })
 
-  homepage = () => {
+  const homepage = () => {
     return <HomePage />
   }
   
-  work = () => {
-    return <Work/>
-  }
+  // const work = () => {
+  //   return <Work/>
+  // }
 
-  fun = () => {
+  const fun = () => {
     return <Fun/>
   }
   
-  dungeon = () => {
+  const dungeon = () => {
     return <Dungeon/>
   }
   
-  now = () => {
+  const now = () => {
     return <Now/>
   }
   
-  notFoundPage = () => {
+  const notFoundPage = () => {
     return <NotFoundPage />
   }
 
-  render(){
-    return (
-        <div className="App">
-          <Logo/>
-          <Switch>
-            <Route exact path="/" render={this.homepage}/>
-            <Route exact from="/index" to="/"/>
-            <Route exact path="/index.html" render={this.homepage}/>
-            {/* <Route exact path="/code" render={this.fun}/> */}
-            <Route exact path="/fun" render={this.fun}/>
-            <Route exact path="/fun/dungeon" render={this.dungeon}/>
-            <Route exact path="/now" render={this.now}/>
-            <Route exact path="/notfound" render={this.notFoundPage}/>
-            <Redirect from="/*" to="/notfound"/>
-          </Switch>
-        </div>
-    );
-  }
+  return (
+      <div className="App">
+        <Logo/>
+        <Switch>
+          <Route exact path="/" render={homepage}/>
+          <Route exact from="/index" to="/"/>
+          <Route exact path="/index.html" render={homepage}/>
+          {/* <Route exact path="/code" render={this.fun}/> */}
+          <Route exact path="/fun" render={fun}/>
+          <Route exact path="/fun/dungeon" render={dungeon}/>
+          <Route exact path="/now" render={now}/>
+          <Route exact path="/notfound" render={notFoundPage}/>
+          <Redirect from="/*" to="/notfound"/>
+        </Switch>
+      </div>
+  );
 }
 
 export default App;

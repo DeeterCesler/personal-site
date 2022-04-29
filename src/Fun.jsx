8dd7e249241d20100f040ba5ac2d0bc47aef193c
d <me@deetercesler.com>
2022-04-28 20:53:39 -0400

    hide everything until I know it is working

--- src/Fun.jsx
+++ src/Fun.jsx
@@ -6,18 +6,18 @@ const Fun = () => {
             <div className="container">
                 <h1 className="header">FUN</h1>
                 <div className="readability">
-                    <div className="item">
+                    {/* <div className="item">
                         <h3>STOCK CHECKER</h3>
                         <p>text a stock ticker like <b>TSLA</b> to <i>+1 (224) 347-1019</i> to get the most recent price of that security</p>
-                    </div>
+                    </div> */}
                     <div className="item">
                         <h3><a className="link" href="http://dungeon-crawler.freeoda.com/">DUNGEON CRAWLER</a></h3>
-                        <p>browser-based game I made while learning jQuery. keyboard required</p>
+                        <p>keyboard required (AKA desktop only). browser-based game I made back in boot camp while learning jQuery</p>
                     </div>
-                    <div className="item">
+                    {/* <div className="item">
                         <h3><a className="link" href="https://happy-minsky-a19081.netlify.app/">LASER EYES MAKER</a></h3>
                         <p>add laser eyes to your profile pic</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>

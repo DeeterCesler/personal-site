20302c954a56b7ffabcee9da9cea1cfc2bcd7544
d <me@deetercesler.com>
2022-06-23 20:51:07 -0400

    reformat with linter, and also add Rust crate to my "fun" section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import HomePage from './HomePage';
+import React, { useEffect } from 'react'
+import './App.css'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import HomePage from './HomePage'
 import Logo from './Logo'
 // import Work from './Work';
-import Fun from './Fun';
-import Dungeon from './FUN/dungeon';
-import Now from './Now';
-import ReactGA from 'react-ga';
-import NotFoundPage from './NotFoundPage';
-import StartupsVersus from './StartupsVersus';
-ReactGA.initialize('UA-110417068-2');
-ReactGA.pageview(window.location.pathname + window.location.search);
+import Fun from './Fun'
+import Dungeon from './FUN/dungeon'
+import Now from './Now'
+import ReactGA from 'react-ga'
+import NotFoundPage from './NotFoundPage'
+import StartupsVersus from './StartupsVersus'
+ReactGA.initialize('UA-110417068-2')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 const consoleArt = `
 %c
@@ -28,7 +28,7 @@ const consoleArt = `
 ██║     ██╔══╝  ╚════██║██║     ██╔══╝  ██╔══██╗ 
 ╚██████╗███████╗███████║███████╗███████╗██║  ██║ 
  ╚═════╝╚══════╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝ 
-                                                 `;
+                                                 `
 
 const consoleMessage = `
 %cWelcome! %cHappy you're here. 
@@ -37,16 +37,18 @@ If you're checking the console, you're probably another developer seeing who I a
 
 I will share any code you see that you're interested, if you ask!
 
-`;
+`
 
-
-function App(){
-
-  useEffect(()=>{
-    console.log(consoleArt, 'color: #3e7bff', 'color: #bc76ff');
-    console.log(consoleMessage,`
+function App() {
+  useEffect(() => {
+    console.log(consoleArt, 'color: #3e7bff', 'color: #bc76ff')
+    console.log(
+      consoleMessage,
+      `
       font-weight: 900;
-    `,'font-weight: 500;');
+    `,
+      'font-weight: 500;',
+    )
   })
 
   const homepage = () => {
@@ -54,22 +56,22 @@ function App(){
   }
 
   return (
-      <div className="App">
-        <Logo/>
-        <Switch>
-          <Route exact path="/" render={homepage}/>
-          <Route exact from="/index" to="/"/>
-          <Route exact path="/index.html" render={homepage}/>
-          {/* <Route exact path="/work" render={Work}/> */}
-          <Route exact path="/fun" render={Fun}/>
-          <Route exact path="/fun/dungeon" render={Dungeon}/>
-          <Route exact path="/now" render={Now}/>
-          <Route exact path="/notfound" render={NotFoundPage}/>
-          <Route exact path="/startups-vs-big-tech" render={StartupsVersus}/>
-          <Redirect from="/*" to="/notfound"/>
-        </Switch>
-      </div>
-  );
+    <div className="App">
+      <Logo />
+      <Switch>
+        <Route exact path="/" render={homepage} />
+        <Route exact from="/index" to="/" />
+        <Route exact path="/index.html" render={homepage} />
+        {/* <Route exact path="/work" render={Work}/> */}
+        <Route exact path="/fun" render={Fun} />
+        <Route exact path="/fun/dungeon" render={Dungeon} />
+        <Route exact path="/now" render={Now} />
+        <Route exact path="/notfound" render={NotFoundPage} />
+        <Route exact path="/startups-vs-big-tech" render={StartupsVersus} />
+        <Redirect from="/*" to="/notfound" />
+      </Switch>
+    </div>
+  )
 }
 
-export default App;
+export default App

b97e9d9cef615416c22f1a7ffdc91320d0bd6aac
d <me@deetercesler.com>
2024-03-10 17:02:30 -0400

    update /now

--- src/Pages/Now.jsx
+++ src/Pages/Now.jsx
@@ -6,14 +6,12 @@ const Now = () => {
             <div className="container">
                 <h1 className="header">NOW</h1>
                 <div className="mini-spacer"/>
+                <p><i>Updated March 10, 2024</i></p>
                 <div className="mini-spacer"/>
                 <div className="mini-spacer"/>
                 <div className="main-text">
                     <p className="now-text"><span role="img" aria-label="man laptop emoji">👨‍💻</span> developing an app to help you complete goals with friends</p>
-                    <p className="now-text"><span role="img" aria-label="cog emoji">⚙️</span> introducing newbies to programming 
-                    <br/>
-                    <br/>
-                    (if you're a code newbie, you might be interested in a <a className="white" href="https://www.youtube.com/watch?v=QKXwNPkQeWA&lc=UgxJoW2T0M-_U6V9BhF4AaABAg.9SeSSJyfTQw9Y25tlLcT0f&ab_channel=DeeterCesler">review of my General Assembly experience</a>)</p>
+                    <p className="now-text"><span role="img" aria-label="seedling emoji">🌱</span> starting a garden</p>
                     <br/>
                 </div>
                 <div className="mini-spacer"/>

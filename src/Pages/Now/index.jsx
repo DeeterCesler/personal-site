a3d823d82045183d0ab5bd3c1885b5c67c77bb6f
Deeter Cesler <deeter.cesler@gmail.com>
2025-03-26 17:04:11 -0400

    clean up blog

--- src/Pages/Now/index.jsx
+++ src/Pages/Now/index.jsx
@@ -9,11 +9,11 @@ const Now = () => {
                 <div className="container now">
                     <h1 className="header">NOW</h1>
                     <div className="mini-spacer"/>
-                    <p><i>Updated March 24, 2025</i></p>
+                    <p><i>Updated March 26, 2025</i></p>
                     <div className="mini-spacer"/>
                     <div className="mini-spacer"/>
                     <div className="main-text">
-                        <p className="now-text"><span role="img" aria-label="seedling emoji">✏️</span> another copywriting contract</p>
+                        <p className="now-text"><span role="img" aria-label="seedling emoji">✏️</span> another copywriting contract for <a className="link" target="_blank" rel="noreferrer" href="https://www.chca-oh.org/">Cincinnati Hills Christian Academy</a></p>
                         <p className="now-text"><span role="img" aria-label="man laptop emoji">👨‍💻</span> developing an app to help you complete goals with friends</p>
                         <br/>
                     </div>

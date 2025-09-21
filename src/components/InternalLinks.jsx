a6b50dcbfc057a4214ae62334400a10b5566417f
Deeter Cesler <me@deetercesler.com>
2025-09-21 19:41:16 -0400

    attempt to add haptic feedback

--- src/components/InternalLinks.jsx
+++ src/components/InternalLinks.jsx
@@ -4,11 +4,24 @@ import { useTranslation } from "react-i18next";
 const InternalLinks = () => {
     const { t } = useTranslation();
 
+    const handleLinkClick = (e) => {
+        if (navigator.vibrate) {
+            try {
+                navigator.vibrate(200);
+                console.log('Haptic feedback triggered');
+            } catch (error) {
+                console.error('Haptic feedback failed:', error);
+            }
+        } else {
+            console.log('navigator.vibrate not supported');
+        }
+    };
+
     return (
         <div className="links">
-            <p className="link-p"><Link className="link" style= {{ textDecoration: "none" }} to="/now">{t('internalLinks.now')}</Link></p>
-            <p className="link-p"><Link className="link" style= {{ textDecoration: "none" }} to="/work">{t('internalLinks.work')}</Link></p>
-            <p className="link-p"><Link className="link" style= {{ textDecoration: "none" }} to="/blog">{t('internalLinks.blog')}</Link></p>
+            <p className="link-p"><Link className="link" style= {{ textDecoration: "none" }} to="/now" onClick={handleLinkClick}>{t('internalLinks.now')}</Link></p>
+            <p className="link-p"><Link className="link" style= {{ textDecoration: "none" }} to="/work" onClick={handleLinkClick}>{t('internalLinks.work')}</Link></p>
+            <p className="link-p"><Link className="link" style= {{ textDecoration: "none" }} to="/blog" onClick={handleLinkClick}>{t('internalLinks.blog')}</Link></p>
         </div>
     )
 }
